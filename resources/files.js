daf74f9ac4a1ce6fd738ec6642f6d94e909ee98d
Felipe Adamoli <felipeadamoli@gmail.com>
2017-09-15 16:41:10 -0300

    including sidenav

---
+++ resources/files.js
@@ -0,0 +1,24 @@
+function openNav() {
+		    document.getElementById("mySidenav").style.width = "100%";
+		    isOpen = true;
+		}
+
+		function closeNav() {
+			isOpen = false;
+			for(i=0;i<links.length;i++){
+				links[i].style.color="black";
+			}
+		    document.getElementById("mySidenav").style.width = "0";
+		}
+
+		var i, isOpen, links = document.getElementsByClassName('linked');
+		document.getElementById("mySidenav").addEventListener("transitionend", myFunction);
+
+
+		function myFunction() {
+			if(!isOpen) return;
+
+			for(i=0;i<links.length;i++){
+				links[i].style.color="white";
+			}
+		};
